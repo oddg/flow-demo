93fccd634cbbd689a82ce08784c89fe3bab6baf2
oddg <odegevigney@gmail.com>
2017-08-24 01:23:05 -0700

    Bad function call

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ const Plan = require('./plan.js');
 const Invoice = require('./invoice.js');
 const Addon = require('./addon.js');
 
-let amount = Invoice.getAmount(10, Plan.LOW_VOLUME, Addon.CHEAP);
+let amount = Invoice.getAmount(10, Plan.LOW_VOLUME, 'chip');
 console.log(amount);
