a900757c93cccff7d48c56a8561ab57d7714210a
oddg <odegevigney@gmail.com>
2017-08-24 01:26:18 -0700

    Fix bug

--- test.js
+++ test.js
@@ -4,5 +4,5 @@ const Plan = require('./plan.js');
 const Invoice = require('./invoice.js');
 const Addon = require('./addon.js');
 
-let amount = Invoice.getAmount(10, Plan.LOW_VOLUME, 'chip');
+let amount = Invoice.getAmount(10, Plan.LOW_VOLUME, Addon.CHEAP);
 console.log(amount);
