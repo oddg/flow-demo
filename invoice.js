9bae725146b9fd8a81f663487dbb6207c2bf987d
oddg <odegevigney@gmail.com>
2017-08-24 01:13:06 -0700

    Introduce bug

--- invoice.js
+++ invoice.js
@@ -4,7 +4,7 @@ const Addon = require('./addon.js');
 
 function getAmount(nbVisits /*: number*/ , plan /*: PlanT*/, addon /*: AddonT */)/*: number */ {
   let perVisitCost = Plan.getPrice(plan);
-  let addonCost = Addon.getPrice(addon);
+  let addonCost = Addon.getPrice(plan);
   return nbVisits * perVisitCost + addonCost;
 }
 
